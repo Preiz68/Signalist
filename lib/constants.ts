0e11e1c9d4df50e2572b0d2ad2fd13be027801fd
Akoji Praise <akojiomede75@gmail.com>
2025-12-23 00:14:49 +0100

    Implement header

---
+++ lib/constants.ts
@@ -0,0 +1,16 @@
+const NAV_ITEMS = [
+  {
+    title: "Dashboard",
+    href: "/",
+  },
+  {
+    title: "Search",
+    href: "/search",
+  },
+  {
+    title: "Watchlist",
+    href: "/watchlist",
+  },
+];
+
+export default NAV_ITEMS;
