f2541ac1f6d415b237d329481022f15af67a8ee5
Akoji Praise <akojiomede75@gmail.com>
2025-12-24 03:13:44 +0100

    Implement database setup

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -58,6 +58,7 @@ const SignUp = () => {
         <InputField
           name="email"
           label="Email"
+          type="email"
           placeholder="contact@jsmastery.com"
           register={register}
           error={errors.email}
